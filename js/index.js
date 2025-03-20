2536159656976e304c3089d38a7e3fe3c20080cc
Kendall <kendalldoescoding@gmail.com>
2025-03-20 16:32:17 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ var today = new Date();
 var date = today.getDate();
 var month = today.getMonth() + 1;
 var year = today.getFullYear();
-// document.write(month + '/' + date + '/' + year);
+// document.write(date + '/' + month + '/' + year);
 
 const mainElement = document.getElementById("main")
 mainElement.innerText = date + '/' + month + '/' + year
